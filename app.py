b657011d924051413074017cdd16292b60b23d09
matthieu-hackwitharts <74831212+matthieu-hackwitharts@users.noreply.github.com>
2024-10-23 12:05:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    for name, value in os.environ.items():
+        print("{0}: {1}".format(name, value))
     return "Hello, Azure!"
 
 if __name__ == '__main__':
